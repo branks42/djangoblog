ed5d663b86b7d1fc90fad0fbbeaa34efc72d6537
Justin Branco <justin.brancode@gmail.com>
2018-06-01 16:33:58 -0400

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
+# Create models here.
 class Posts(models.Model):
 	title = models.CharField(max_length=200)
 	body = models.TextField()
