9d6f4c3d91ee9770d8fba14a4aba93c12a6542f6
Justin Branco <justin.brancode@gmail.com>
2018-06-01 16:34:49 -0400

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+# These url patterns only work on old versions of Django
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^/details/(?P<id>\d+)/$', views.details, name='details')
